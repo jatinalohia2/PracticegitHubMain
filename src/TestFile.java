22d03272e47a1e619b0a8b2b64a099aaff7e9145
jatinalohia2 <jatinalohia2@gmail.com>
2025-09-27 09:42:52 +0530

    first commit

---
+++ src/TestFile.java
@@ -0,0 +1,16 @@
+
+public class TestFile {
+
+	public static void main(String[] args) {
+		
+		String name = "jatin";
+		
+		if (name.equals("jatin")) {
+			System.out.println("yes this works");
+		}else {
+			System.out.println("this not works");
+		}
+		
+		
+	}
+}
