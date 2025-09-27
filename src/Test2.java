59c15599f08f3437ffe7c5cad80df0ce14b4b74f
jatinalohia2 <jatinalohia2@gmail.com>
2025-09-27 10:47:57 +0530

    addtional changes commit

--- src/Test2.java
+++ src/Test2.java
@@ -3,6 +3,11 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("this is another test case .....");
-	}
+String name = "jatin";
+		
+		if (name.equals("jatin") || name == "y") {
+			System.out.println("yes this works");
+		}else {
+			System.out.println("this not works");
+		}	}
 }
